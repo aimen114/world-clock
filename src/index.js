3ccd3fe3b75e4aa3fe9b3ac6b16e6460bdfe7bd6
aimen114 <aimenzafar095@gmail.com>
2023-10-15 01:52:23 +0500

    added vancouver time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-
+setInterval(function () {
+  let vancouverDateElement = document.querySelector("#v-date");
+  let vancouverTimeElement = document.querySelector("#v-time");
+  let vancouvermoment = moment().tz("America/Vancouver");
+  vancouverDateElement.innerHTML = vancouvermoment.format("MMMM Do YYYY");
+  vancouverTimeElement.innerHTML = vancouvermoment.format(
+    "hh : mm : ss [<small>]a[</small>]"
+  );
+}, 1000);
