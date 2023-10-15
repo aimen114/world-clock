77593b28c25fcc435483f33871934dad0dd7aa5f
aimen114 <aimenzafar095@gmail.com>
2023-10-15 19:22:32 +0500

    added functionality to select element

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-setInterval(function () {
-  let vancouverDateElement = document.querySelector("#v-date");
-  let vancouverTimeElement = document.querySelector("#v-time");
-  let vancouvermoment = moment().tz("America/Vancouver");
-  vancouverDateElement.innerHTML = vancouvermoment.format("MMMM Do YYYY");
-  vancouverTimeElement.innerHTML = vancouvermoment.format(
-    "hh : mm : ss [<small>]a[</small>]"
-  );
-}, 1000);
+function displayCities() {
+  let halifaxElement = document.querySelector("#halifax-date");
+  if (halifaxElement) {
+    let halifaxTimeElement = document.querySelector("#halifax-time");
+    let halifax_moment = moment().tz("America/Halifax");
+    halifaxElement.innerHTML = halifax_moment.format("MMMM Do YYYY");
+    halifaxTimeElement.innerHTML = halifax_moment.format(
+      "hh : mm : ss [<small>]a[</small>]"
+    );
+  }
+  let winnipegElement = document.querySelector("#winnipeg-date");
+  if (winnipegElement) {
+    let winnipegTimeElement = document.querySelector("#winnipeg-time");
+    let winnipeg_moment = moment().tz("America/Winnipeg");
+    winnipegElement.innerHTML = winnipeg_moment.format("MMMM Do YYYY");
+    winnipegTimeElement.innerHTML = winnipeg_moment.format(
+      "hh : mm : ss [<small>]a[</small>]"
+    );
+  }
+}
+
+setInterval(displayCities, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  console.log(cityTime.format("MMMM Do YYYY"));
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+   <div class="row">
+          <div class="col">
+            <h2>${cityName}</h2>
+            <div class="date" id="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="col text-end">
+            <div class="time" id="time">${cityTime.format(
+              "hh : mm : ss"
+            )} <small> ${cityTime.format("A")}</small></div>
+          </div>
+        </div>`;
+}
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
